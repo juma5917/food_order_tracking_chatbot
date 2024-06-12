7170bf2c4961c5e3153d42112ba5a82a42d209d6
Juma Samwel <73012782+juma5917@users.noreply.github.com>
2024-06-12 14:08:46 +0300

    Add files via upload

---
+++ Jumabot.py
@@ -0,0 +1,68 @@
+from flask import Flask, render_template, request, jsonify
+import numpy as np
+import tensorflow as tf
+import json
+import nltk
+from nltk.stem import WordNetLemmatizer
+import pickle
+import random
+
+app = Flask(__name__)
+
+lemmatizer = WordNetLemmatizer()
+
+# Load model and data
+model = tf.keras.models.load_model('chatbot_jumamodel.keras')
+intents = json.loads(open('chatbot/intents.json').read())
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+def bow(sentence, words, show_details=True):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for s in sentence_words:
+        for i, w in enumerate(words):
+            if w == s:
+                bag[i] = 1
+                if show_details:
+                    print(f"found in bag: {w}")
+    return np.array(bag)
+
+def predict_class(sentence, model):
+    p = bow(sentence, words, show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list
+
+def get_response(ints, intents_json):
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            return random.choice(i['responses'])
+            break
+    return result
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/get", methods=["POST"])
+def chatbot_response():
+    msg = request.form["msg"]
+    ints = predict_class(msg, model)
+    res = get_response(ints, intents)
+    return jsonify({"response": res})
+
+if __name__ == "__main__":
+    app.run(debug=True)
